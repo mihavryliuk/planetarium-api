33e02401944358eb4342a5940f3c5942715a20ee
Misha Havryliuk <misharob31415@gmail.com>
2024-10-13 23:43:21 +0300

    add ticket

--- planetarium/models.py
+++ planetarium/models.py
@@ -3,6 +3,7 @@ import uuid
 
 from django.db import models
 from django.utils.text import slugify
+from rest_framework.exceptions import ValidationError
 
 from planetarium_api import settings
 
@@ -34,8 +35,8 @@ class AstronomyShow(models.Model):
 
 class PlanetariumDome(models.Model):
     name = models.CharField(max_length=255)
-    rows = models.IntegerField()
-    seats_in_row = models.IntegerField()
+    rows = models.PositiveIntegerField()
+    seats_in_row = models.PositiveIntegerField()
 
     @property
     def capacity(self) -> int:
@@ -68,3 +69,68 @@ class Reservation(models.Model):
 
     def __str__(self):
         return f"Reservation by {self.user} on {self.created_at}"
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    show_session = models.ForeignKey(
+        ShowSession, on_delete=models.CASCADE, related_name="tickets"
+    )
+    reservation = models.ForeignKey(
+        Reservation, on_delete=models.CASCADE, related_name="tickets"
+    )
+
+    @staticmethod
+    def validate_ticket(row, seat, planetarium_dome, error_to_raise):
+        for (ticket_attr_value,
+             ticket_attr_name,
+             planetarium_dome_attr_name) in [
+            (row, "row", "rows"),
+            (seat, "seat", "seats_in_row"),
+        ]:
+            count_attrs = getattr(planetarium_dome,
+                                  planetarium_dome_attr_name)
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name:
+                            f"{ticket_attr_name} "
+                            f"number must be in available range: "
+                            f"(1, {planetarium_dome_attr_name}): "
+                            f"(1, {count_attrs})"
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.row,
+            self.seat,
+            self.show_session.planetarium_dome,
+            ValidationError,
+        )
+
+    def save(
+            self,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None,
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(
+            force_insert, force_update, using, update_fields
+        )
+
+    def __str__(self):
+        return (f"Ticket: {self.id},"
+                f"(row: {self.row}, seat: {self.seat})."
+                f"Session info: {self.show_session}\n")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["show_session", "reservation", "row", "seat"],
+                name="unique_ticket")
+        ]
+        ordering = ("row", "seat")
