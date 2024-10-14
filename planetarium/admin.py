37222189621d0c5d160444145995e8a054aa08e8
Misha Havryliuk <misharob31415@gmail.com>
2024-10-14 17:48:41 +0300

    admin register + urls

--- planetarium/admin.py
+++ planetarium/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from planetarium.models import (
+    ShowTheme,
+    PlanetariumDome,
+    AstronomyShow,
+    ShowSession,
+    Reservation,
+    Ticket,
+)
 
-# Register your models here.
+admin.site.register(ShowTheme)
+admin.site.register(PlanetariumDome)
+admin.site.register(AstronomyShow)
+admin.site.register(ShowSession)
+admin.site.register(Reservation)
+admin.site.register(Ticket)
