774e1ccae7225b22ab4127e66837e6fe7b1f7f13
Misha Havryliuk <misharob31415@gmail.com>
2024-10-14 10:06:15 +0300

    AstronomyShow models

--- planetarium/serializers.py
+++ planetarium/serializers.py
@@ -1,9 +1,58 @@
 from rest_framework import serializers
 
-from planetarium.models import ShowTheme
+from planetarium.models import ShowTheme, PlanetariumDome, AstronomyShow
 
 
 class ShowThemeSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShowTheme
         fields =("id", "name")
+
+
+class PlanetariumDomeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PlanetariumDome
+        fields = ("id", "name", "rows", "seats_in_row", "capacity")
+
+
+class AstronomyShowSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AstronomyShow
+        fields = (
+            "id",
+            "title",
+            "description",
+            "duration",
+            "themes",
+        )
+
+
+class AstronomyShowListSerializer(AstronomyShowSerializer):
+    themes = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="name"
+    )
+
+    class Meta:
+        model = AstronomyShow
+        fields = ("id", "title", "themes", "image")
+
+
+class AstronomyShowDetailSerializer(AstronomyShowSerializer):
+    themes = serializers.StringRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = AstronomyShow
+        fields = (
+            "id",
+            "title",
+            "duration",
+            "description",
+            "themes",
+            "image",
+        )
+
+
+class AstronomyShowImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AstronomyShow
+        fields = ("id", "image")
